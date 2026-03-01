404687b8bacf87490b69547079d30afda260b212
ColinThePanda <urboycolinthepanda@gmail.com>
2026-03-01 15:49:57 -0500

    test base

--- src/lib/base.ts
+++ src/lib/base.ts
@@ -1,2 +1,4 @@
 const isGitHub = import.meta.env.GITHUB_ACTIONS === "true";
+console.log("GITHUB_ACTIONS:", import.meta.env.GITHUB_ACTIONS);
+console.log("base:", isGitHub ? "/ACSLContestSite" : "");
 export const base = isGitHub ? "/ACSLContestSite" : "";
