24fb211237e1624f0027be5ad67803df9a31e56f
Colin Politi <urboycolinthepanda@gmail.com>
2026-03-01 04:33:57 -0500

    add wiki

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,17 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   vite: {
     plugins: [tailwindcss()],
     resolve: {
@@ -16,5 +25,11 @@ export default defineConfig({
     },
   },
 
-  integrations: [react()]
+  integrations: [
+    react(),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    })
+  ],
 });
